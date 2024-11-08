95df6d0b8acfe749ef8b3cbe10e1669e8ee66e66
Camila <camila.gomez@fceqyn.unam.edu.ar>
2024-11-08 16:11:30 -0300

    arreglos en el calendario y redireccion al dia del calendario, solo vista

--- backend/src/main/java/com/API/REST/controlador/cliente/CalendarioController.java
+++ backend/src/main/java/com/API/REST/controlador/cliente/CalendarioController.java
@@ -3,6 +3,7 @@ package com.API.REST.controlador.cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CalendarioController {
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("contenidoCliente", "cliente/calendario");
+        model.addAttribute("contenidoCliente", "cliente/miCalendario/calendario");
+        return "cliente/principal";
+    }
+
+    @GetMapping("/{year}-{month}-{day}")
+    public String verDia(@PathVariable int year,
+                         @PathVariable int month,
+                         @PathVariable int day,
+                         Model model) {
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("contenidoCliente", "cliente/miCalendario/dia");
         return "cliente/principal";
     }
 }
